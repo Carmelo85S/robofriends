cef22b87f2e0be9627b367c18fd485c8699258db
Carmelo85S <carmelo.salis85@gmail.com>
2024-03-01 17:29:59 +0100

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         <h1>Loading</h1> :
         (
             <div className = 'tc'>
-                <h1 className ='f1'>Robofriend</h1>
+                <h1 className ='f1'>Robofriends</h1>
                 <SearchBox searchChange = {this.onSearchchange} />
                 <Scroll>    
                     <CardList robots ={ filteredRobots} />
